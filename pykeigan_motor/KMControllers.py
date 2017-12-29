99433b16c339e8b7cdc55a4863dabc02315912d0
Tomohiro Takata <takata@innovotion.co.jp>
2017-12-29 17:59:47 +0900

    concealed the read_setting_value method

--- pykeigan_motor/KMControllers.py
+++ pykeigan_motor/KMControllers.py
@@ -444,7 +444,7 @@ class Controller:
         """
         command=b'\xE0'
         values=uint8_t2bytes(ledState)+uint8_t2bytes(red)+uint8_t2bytes(green)+uint8_t2bytes(blue)
-        self.run_command(command+identifier+values+crc16,"motor_led")
+        self.run_command(command+identifier+values+crc16,"motor_control")
 
     # IMU
     def enableIMU(self,identifier=b'\x00\x00',crc16=b'\x00\x00'):
@@ -543,13 +543,14 @@ class BLEController(Controller):
         self.position=bytes2float(ba[0:4])
         self.velocity=bytes2float(ba[4:8])
         self.torque=bytes2float(ba[8:12])
-        return self.position,self.velocity,self.torque
+        return (self.position,self.velocity,self.torque)
     
     def read_imu_mesurement(self):
         """
         Get the x,y,z axis acceleration, temperature, and anguler velocities around x,y,z axis
         and store them to 'accel_x', 'accel_y', 'accel_z' in g(9.80665 m/s^2), 'temp' in degree Celsius, 'gyro_x', 'gyro_y', and 'gyro_z' in rad/sec.
         """
+        self.enableIMU()
         ba=self.dev.readCharacteristic(self.motor_imu_measurement_handle)
         self.accel_x=bytes2int16_t(ba[0:2])* 2.0 / 32767
         self.accel_y=bytes2int16_t(ba[2:4])* 2.0 / 32767
@@ -558,7 +559,7 @@ class BLEController(Controller):
         self.gyro_x=bytes2int16_t(ba[8:10])* 0.00013316211
         self.gyro_y=bytes2int16_t(ba[10:12])* 0.00013316211
         self.gyro_z=bytes2int16_t(ba[12:14])* 0.00013316211
-        return self.accel_x,self.accel_y,self.accel_z,self.temp,self.gyro_x,self.gyro_y,self.gyro_z
+        return (self.accel_x,self.accel_y,self.accel_z,self.temp,self.gyro_x,self.gyro_y,self.gyro_z)
     
     def __read_float_data(self,ba):
         return bytes2float(ba[4:8])
@@ -567,9 +568,9 @@ class BLEController(Controller):
         return bytes2uint8_t(ba[4])
     
     def __read_rgb_data(self,ba):
-        return (bytes2uint8_t(ba[4]),bytes2uint8_t(ba[5]),bytes2uint8_t(ba[6]))
+        return (ba[4],ba[5],ba[6])
     
-    def read_setting_value(self,comm):
+    def __read_setting_value(self,comm):
         float_value_comms=[0x02,0x03,0x07,0x08,0x0E,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E]
         valid_comms=[0x05,0x3A]
         valid_comms.extend(float_value_comms)
@@ -585,7 +586,45 @@ class BLEController(Controller):
             return self.__read_uint8_data(ba)
         if comm==0x3A:
             return self.__read_rgb_data(ba)
-        
+    
+    def read_maxSpeed(self):
+        return self.__read_setting_value(0x02)
             
-        
-        
+    def read_minSpeed(self):
+        return self.__read_setting_value(0x03)
+    
+    def read_curveType(self):
+        return self.__read_setting_value(0x05)
+    
+    def read_acc(self):
+        return self.__read_setting_value(0x07)
+    
+    def read_dec(self):
+        return self.__read_setting_value(0x08)
+    
+    def read_maxTorque(self):
+        return self.__read_setting_value(0x0E)
+    
+    def read_qCurrentP(self):
+        return self.__read_setting_value(0x18)
+    
+    def read_qCurrentI(self):
+        return self.__read_setting_value(0x19)
+    
+    def read_qCurrentD(self):
+        return self.__read_setting_value(0x1A)
+    
+    def read_speedP(self):
+        return self.__read_setting_value(0x1B)
+    
+    def read_speedI(self):
+        return self.__read_setting_value(0x1C)
+    
+    def read_speedD(self):
+        return self.__read_setting_value(0x1D)
+    
+    def read_positionP(self):
+        return self.__read_setting_value(0x1E)
+    
+    def read_ownColor(self):
+        return self.__read_setting_value(0x3A)
