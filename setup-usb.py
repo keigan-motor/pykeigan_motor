5d9e048d51705cda9905111cdb5f6c206b036eac
Tomohiro Takata <takata@innovotion.co.jp>
2019-06-01 09:13:27 +0900

    python 2 support

--- setup-usb.py
+++ setup-usb.py
@@ -1,23 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+from os import path
 
-
-with open('README.rst') as f:
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     readme = f.read()
-
-with open('MIT-LICENSE') as f:
-    license = f.read()
 
 setup(
     name='pykeigan_motor_usb',
-    version='2.0.0',
-    description='Python module for Keigan Motors on USB',
+    version='2.1.0',
+    description='Python Library for Keigan Motors on USB',
     long_description=readme,
     install_requires=['pyserial'],
     author='Tomohiro Takata, Hiroshi Harada',
     author_email='takata@innovotion.co.jp, harada@keigan.co.jp',
     url='https://github.com/keigan-motor/pykeigan_motor',
-    license=license,
+    license='MIT-LICENSE',
     packages=find_packages(exclude=('tests', 'docs'))
 )
