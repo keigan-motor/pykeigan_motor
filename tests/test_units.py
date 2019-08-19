eeda2aee06654b18fa467b54944599649429ffc4
okhiroyuki <ok.okada.hiroyuki@gmail.com>
2019-08-19 23:24:37 +0900

    add test

---
+++ tests/test_units.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('../pykeigan')
+from pykeigan.utils import *
+
+def test_float2bytes():
+    result = float2bytes(1);
+    assert result == b'?\x80\x00\00'
+
+def test_bytes2float():
+    result = bytes2float(b'?\x80\x00\00');
+    assert result == 1
