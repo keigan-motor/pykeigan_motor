e2ef855c707be1cae5d58e1b8391a6346bc3cf60
wingcloud <takata.tomohiro@gmail.com>
2019-08-21 10:42:56 +0900

    Update test_units.py

--- tests/test_units.py
+++ tests/test_units.py
@@ -8,3 +8,21 @@ def test_float2bytes():
 def test_bytes2float():
     result = bytes2float(b'?\x80\x00\00');
     assert result == 1
+        
+def test_uint8_t2bytes(self):
+    assert uint8_t2bytes(23)==b'\x17'
+    
+def test_bytes2uint8_t(self):
+    assert bytes2uint8_t(b'\x17')==23
+    
+def test_uint16_t2bytes(self):
+    assert uint16_t2bytes(23)==b'\x00\x17'
+        
+def test_bytes2uint16_t(self):
+    assert bytes2int16_t(b'\x00\x17')==23
+        
+def test_bytes2int16_t(self):
+    assert bytes2int16_t(b'\xff\xff')==-1
+    
+def test_uint32_t2bytes(self):
+    assert uint32_t2bytes(16**6)==b'\x01\x00\x00\x00'
