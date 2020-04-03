6f39ccf8e58db1a3167aa176284e953d557ca122
harada-hiroshi <harada@orange-soft.jp>
2020-04-03 20:35:36 +0900

    When running with python2.x, the problem of high CPU load

--- pykeigan/usbcontroller.py
+++ pykeigan/usbcontroller.py
@@ -101,7 +101,7 @@ class USBController(base.Controller):
 
     def __serial_schedule_worker(self):
         while True:
-            time.sleep(100 / 1000)  # 100ms
+            time.sleep(0.1)  # 100ms
             e_res = self.__read_serial_data()
             if e_res or self.auto_serial_reading == False:  # 例外発生でスレッド停止
                 self.auto_serial_reading = False
