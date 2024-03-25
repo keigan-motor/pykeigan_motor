4d064c722eca88aebaf5e6177fc679699de51002
Hiroki Sugimura <sugimura@bitset.jp>
2024-03-25 11:21:33 +0900

    serial初期化失敗時の再試行処理を修正

--- pykeigan/usbcontroller.py
+++ pykeigan/usbcontroller.py
@@ -29,14 +29,7 @@ class USBController(base.Controller):
         self.try_reconnect = False
         self.reconn_err_cnt = 0
         #try to connect to serial port
-        try:
-            self.serial = serial.Serial(port, baud, 8, 'N', 1, None, False, True, write_timeout=0.1)
-        except Exception as e:
-            print('Error occured while trying to connect to serial port. Please recheck you USB connection.')
-            print('attemping to reinit... ')
-            time.sleep(3)
-            self.reinit()
-            return
+        self.serial = self.__init_serial(port, baud)
             
         self.on_motor_measurement_value_cb = False
         self.on_motor_imu_measurement_cb = False
@@ -64,15 +57,15 @@ class USBController(base.Controller):
             print('...reconnecting...')
             #self.disconnect()
             #self.__init__()
-            
-    def reinit(self):
-        try:
-            print('\n...reiniting...\n')
-            self.__init__()
-            return
-        except Exception as e:
-            time.sleep(3)
-            self.reinit()
+
+    def __init_serial(self, port, baud):
+        while True:
+            try:
+                return serial.Serial(port, baud, 8, 'N', 1, None, False, True, write_timeout=0.1)
+            except Exception as e:
+                print('Error occured while trying to connect to serial port. Please recheck you USB connection.')
+                print('attemping to reinit... ')
+                time.sleep(3)
     
     def is_connected(self):
         return self.serial.isOpen()
